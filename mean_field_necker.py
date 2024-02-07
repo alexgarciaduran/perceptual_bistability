37667f26040735372a77c7d3ff014bbf3177b07d
alexgarciaduran <alexgaduca@gmail.com>
2024-02-07 16:21:10 +0100

    bifurcation analysis, added taylor expansions for solution of MF and for potential

--- mean_field_necker.py
+++ mean_field_necker.py
@@ -16,6 +16,8 @@ from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pylab as pl
 import matplotlib as mpl
+from scipy.optimize import fsolve
+import sympy
 
 
 
@@ -51,6 +53,50 @@ def mean_field(J, num_iter, sigma=1):
     return vec_time
 
 
+def mean_field_stim(J, num_iter, stim, sigma=1):
+    #initialize random state of the cube
+    vec = np.repeat(0.7, 8)
+    vec_time = np.empty((num_iter, 8))
+    vec_time[:] = np.nan
+    for i in range(num_iter):
+        for q in range(8):
+            neighbours = theta[q].astype(dtype=bool)
+            vec[q] = gn.sigmoid(2*(sum(J*(2*vec[neighbours]-1)+stim))+np.random.randn()*sigma) 
+        vec_time[i, :] = vec
+    return vec_time
+
+
+def mean_field_same_q(j_list, stim, num_iter=100):
+    qvls = []
+    for j in j_list:
+        q_val = 0.7
+        all_q = np.empty((num_iter))
+        all_q[:] = np.nan
+        for i in range(num_iter):
+            q_val = gn.sigmoid(6*(j*(2*q_val-1)+stim))
+        qvls.append(q_val)
+    plt.figure()
+    plt.plot(j_list, qvls, color='r')
+    plt.plot(j_list, 1-np.array(qvls), color='k')
+
+
+def plot_mf_sol_stim_bias(j_list, stim=0.1, num_iter=500):
+    pos = []
+    neg = []
+    for j in j_list:
+        v = mean_field_stim(j, num_iter=num_iter, stim=stim, sigma=0)
+        v_mn = v[-1, 0]
+        pos.append(v_mn)
+        neg.append(1-v_mn)
+    plt.figure()
+    plt.plot(j_list, pos, color='r', markersize=2)
+    plt.plot(j_list, neg, color='k', markersize=2)
+    plt.xlabel('J')
+    plt.ylabel('q')
+
+
+def backwards(q, j, beta):
+    return 0.5*(1+ 1/j *(1/6 * np.log(q/(1-q)) - beta))
 
 
 def plot_mf_sol(j_list, num_iter=500):
@@ -83,11 +129,11 @@ def plot_mf_sol_sigma_j(data_folder, j_list, sigma_list, num_iter=2000):
     plt.ylabel('J')
 
 
-def plot_solutions_mfield(j_list):
+def plot_solutions_mfield(j_list, bias=0):
     l = []
     for j in j_list:
-        q = lambda q: gn.sigmoid(6*j*(2*q-1)) - q
-        l.append(fsolve(q,1))
+        q = lambda q: gn.sigmoid(6*j*(2*q-1)+ bias*6) - q 
+        l.append(np.clip(fsolve(q, 0.9), 0, 1))
     plt.axhline(0.5, color='grey', alpha=1, linestyle='--')
     plt.plot(j_list, 1-np.array(l), color='k')
     plt.plot(j_list, l, color='r')
@@ -96,17 +142,155 @@ def plot_solutions_mfield(j_list):
     plt.title('Solutions of the dynamical system')
 
 
-def dyn_sys_mf(q, dt, j, sigma=1):
-    return np.clip(q + dt*(gn.sigmoid(6*j*(2*q-1))-q)+
+
+def plot_solutions_mfield_beta(j_list, beta=0):
+    l = []
+    for j in j_list:
+        q = lambda q: gn.sigmoid(6*(j*(2*q-1) + beta)) - q
+        l.append(np.clip(fsolve(q,1), 0, 1))
+    plt.axhline(0.5, color='grey', alpha=1, linestyle='--')
+    plt.plot(j_list, 1-np.array(l), color='k')
+    plt.plot(j_list, l, color='r')
+    plt.xlabel('J')
+    plt.ylabel('q')
+    plt.title('Solutions of the dynamical system')
+
+
+
+def plot_sols_mf_bias_stim_changing_j(j_list=[0.55, 0.6, 0.65, 0.7, 0.75, 0.8],
+                                      beta=1e-1):
+    fig, ax = plt.subplots(ncols=3, nrows=2)
+    ax = ax.flatten()
+    q = np.arange(0, 1, 0.001)
+    for i_j, j in enumerate(j_list):
+        func = gn.sigmoid(6*(j*(2*q-1)+beta))
+        ax[i_j].plot(q, func, color='k')
+        ax[i_j].plot(q, q, color='r')
+        ax[i_j].set_title('J='+str(round(j, 2)))
+        ax[i_j].set_ylabel(r'$f(q, J)$')
+        ax[i_j].set_xlabel(r'$q$')
+
+
+def plot_crit_J_vs_B(j_list, num_iter=200, beta_list=np.arange(0, 0.1, 0.001)):
+    first_j = []
+    for beta in beta_list:
+        final_q = []
+        for j in j_list:
+            q_fin = 0.65
+            for i in range(num_iter):
+                q_fin = backwards(q_fin, j, beta)
+            final_q.append(q_fin)
+        first_j.append(j_list[~np.isnan(final_q)][0])
+    plt.figure()
+    plt.plot(beta_list, first_j, color='k', linewidth=2)
+    plt.fill_between(beta_list, first_j, 1, color='mistyrose', alpha=0.6)
+    plt.text(0.03, 0.8, '1 repulsor, 2 attractor')
+    plt.text(0.03, 0.2, '1 attractor')
+    plt.fill_between(beta_list, 0, first_j, color='lightcyan', alpha=0.6)
+    plt.ylabel('J*')
+    plt.xlabel('B')
+    plt.ylim(0, max(j_list))
+    plt.xlim(0, max(beta_list))
+
+
+def plot_sols_mf_bias_stim_changing_beta(
+        beta_list=[0, 0.02, 0.04, 0.06, 0.08, 0.1], j=0.5):
+    fig, ax = plt.subplots(ncols=3, nrows=2)
+    ax = ax.flatten()
+    q = np.arange(0, 1, 0.001)
+    for i_j, beta in enumerate(beta_list):
+        func = gn.sigmoid(6*(j*(2*q-1)+beta))
+        ax[i_j].plot(q, func, color='k')
+        ax[i_j].plot(q, q, color='r')
+        ax[i_j].set_title('B='+str(round(beta, 2)))
+        ax[i_j].set_ylabel(r'$f(q, J)$')
+        ax[i_j].set_xlabel(r'$q$')
+
+
+def sol_bias_stim(j, beta):
+    k = 6*j
+    beta = 6*beta
+    return (-beta+k+np.log(k-np.sqrt(k*(k-2))-1))/(2*k),\
+        (-beta+k+np.log(k+np.sqrt(k*(k-2))-1))/(2*k)
+
+
+def sol_mf_stim_taylor(j, beta):
+    k = 6*j
+    b = 6*beta
+    return (1+np.exp(b)-np.exp(-b)*k)/(np.cosh(b)*2-2*k+2)
+
+
+def sol_taylor_beta(j, beta):
+    k = 6*j
+    beta = 6*beta
+    return (((np.exp(beta)+1)**2)/(np.exp(-beta)+1)-np.exp(beta)*k)/\
+        ((np.exp(beta)+1)**2 -2*np.exp(beta)*k)
+
+
+def sigma(x, k, b):
+    return x + x*np.exp(-k * (2*x - 1) - b) - 1
+
+
+def find_solution_sigma(k, b, x0=0.4):
+    solution = fsolve(sigma, x0=x0, args=(k, b), xtol=1e-16)
+    return solution
+
+
+def solution_mf_sigma(ax, j_list, b):
+    colors = ['r', 'k', 'g']
+    sol_04 = []
+    sol_005 = []
+    sol_09 = []
+    for j in j_list:
+        sol_005.append(find_solution_sigma(j*6, b*6, 0.05))
+        sol_04.append(find_solution_sigma(j*6, b*6, 0.4))
+        sol_09.append(find_solution_sigma(j*6, b*6, 0.9))
+    ax.plot(j_list, sol_005, label='b ='+str(b)+', x0=0.05',
+    color=colors[0])
+    ax.plot(j_list, sol_04, label='b ='+str(b)+', x0=0.4',
+    color=colors[1])
+    ax.plot(j_list, sol_09, label='b ='+str(b)+', x0=0.9',
+    color=colors[2])
+    ax.set_ylim(0, 1)
+    ax.legend()
+    ax.set_xlabel('J')
+    ax.set_ylabel('q')
+
+def plot_solution_mf_numeric_solver(j_list=np.arange(0.001, 1, 0.001),
+                                    b_list=[0, 0.025, 0.05,
+                                            0.075, 0.1, 0.2]):
+    fig, ax = plt.subplots(ncols=3, nrows=2)
+    ax = ax.flatten()
+    for a, b in zip(ax, b_list):
+        solution_mf_sigma(ax=a, j_list=j_list, b=b)
+
+
+def solution_for_sigmoid_taylor_order2(j, beta):
+    k = 6*j
+    b = 6*beta
+    numerator = (((2 * np.exp(b) * k**2) / (np.exp(b) + 1)**3) 
+                 - ((2 * np.exp(2 * b) * k**2) / (np.exp(b) + 1)**3) 
+                 - np.sqrt(((4 * np.exp(b) * k**2) / (np.exp(b) + 1)**3) 
+                           - ((4 * np.exp(2 * b) * k**2) / (np.exp(b) + 1)**3) 
+                           - ((8 * np.exp(b) * k**2) / ((np.exp(-b) + 1) * (np.exp(b) + 1)**3)) 
+                           + ((8 * np.exp(2 * b) * k**2) / ((np.exp(-b) + 1) * (np.exp(b) + 1)**3)) 
+                           + ((4 * np.exp(2 * b) * k**2) / (np.exp(b) + 1)**4) 
+                           - ((4 * np.exp(b) * k) / (np.exp(b) + 1)**2) + 1) - ((2 * np.exp(b) * k) / (np.exp(b) + 1)**2) + 1)
+    denominator = 2 * (((2 * np.exp(b) * k**2) / (np.exp(b) + 1)**3) - ((2 * np.exp(2 * b) * k**2) / (np.exp(b) + 1)**3))
+    return numerator / denominator
+
+
+def dyn_sys_mf(q, dt, j, sigma=1, bias=0):
+    return np.clip(q + dt*(gn.sigmoid(6*j*(2*q-1))-q+bias)+
         np.random.randn()*np.sqrt(dt)*sigma, 0, 1)
 
 
-def plot_q_evol_time_noise(j=0.7, dt=1e-3, num_iter=100):
+def plot_q_evol_time_noise(j=0.7, dt=1e-3, num_iter=100, sigma=1):
     q = np.random.rand()
     q_l = []
     # time = np.arange(num_iter)*dt
     for i in range(num_iter):
-        q = np.clip(dyn_sys_mf(q, dt, j, sigma=0.1), 0, 1)
+        q = np.clip(dyn_sys_mf(q, dt, j, sigma=sigma), 0, 1)
         q_l.append(q)
     plt.plot(q_l)
     plt.xlabel('Steps')
@@ -114,13 +298,78 @@ def plot_q_evol_time_noise(j=0.7, dt=1e-3, num_iter=100):
     plt.ylim(-0.05, 1.05)
 
 
-def potential_mf(q, j):
-    return q*q/2 - np.log(1+np.exp(6*j*(2*q-1)))/(12*j)
+def potential_mf(q, j, bias=0):
+    return q*q/2 - np.log(1+np.exp(6*(j*(2*q-1)+bias)))/(12*j) #  + q*bias
 
 
-def plot_potentials_mf(j_list):
-    colormap = pl.cm.Blues(np.linspace(0.2, 1, len(j_list)))
-    colormap_1 = pl.cm.Purples(np.linspace(0.4, 1, len(j_list)))
+def plot_potentials_different_beta(j=0.5, beta_list=[-0.1, -0.05, 0, 0.05, 0.1]):
+    plt.figure()
+    colormap = pl.cm.Blues(np.linspace(0.2, 1, len(beta_list)))
+    q = np.arange(0, 1, 0.001)
+    for i_j, beta in enumerate(beta_list):
+        pot = potential_mf(q, j, beta)
+        plt.plot(q, pot-np.mean(pot), label=np.round(beta, 1), color=colormap[i_j])
+    plt.legend(title='B+b')
+
+
+def mclaurin_expansion_pot(q, j ,b):
+    k = 6*(j+b)
+    order_0 = np.log(1+np.exp(-k))
+    order_1 = (2*k*q)/(np.exp(k)+1)
+    order_2 = (2*np.exp(k)*k*k*q*q)/(np.exp(k)+1)**2
+    order_3 = (4*np.exp(k)*(np.exp(k)-1)*k*k*k*q*q*q)/(3*(np.exp(k)+1)**3)
+    order_4 = (2*np.exp(k)*(-4*np.exp(k)+np.exp(2*k)+1)*k*k*k*k*q*q*q*q)/(3*(np.exp(k)+1)**4)
+    expansion = 0.5*q**2 - (order_0+order_1+order_2+order_3+order_4) / (2*k)
+    return expansion
+
+
+def potential_expansion_at_any_point_order_4(q, j, b, point):
+    k = 6*(j+b)
+    q_centered = q-point
+    expo = np.exp(k*(2*point-1))
+    f_0 = np.log(1+expo)
+    f_1 = 2*k*expo / (1+expo)
+    f_2 = 4*(k**2)*expo / (1+expo)**2
+    f_3 = 8*(k**3) * expo*(expo-1) / (1+expo)**3
+    f_4 = 16 * (k**4) * expo * (np.exp(2*k*(2*point-1))-4*expo+1) / (1+expo)**4
+    expansion = f_0 + f_1*q_centered + f_2*(q_centered**2) / 2 +\
+        f_3*(q_centered**3)/6 + f_4*(q_centered**4)/24
+    return -expansion/(2*k) + 0.5*q**2
+
+
+def potential_expansion_any_order_any_point(q, j, b, order, point):
+    x = sympy.symbols('x')
+    k = 6*(j+b)
+    func_0 = 0.5*x**2-sympy.log(1+sympy.exp(k*(2*x-1)))/(2*k)
+    funcs_diffs = [func_0]
+    for i in range(order):
+        funcs_diffs.append(sympy.diff(funcs_diffs[i], x))
+    coefs = [funcs_diffs[n].subs(x, point) / fact(n) for n in range(order+1)]
+    q_vals = [(q-point)**n for n in range(order+1)]
+    expansion = 0
+    for i in range(order+1):
+        expansion = expansion + (coefs[i]*q_vals[i])
+    return expansion
+    
+
+def fact(n):
+    return np.prod(np.arange(n)+1)
+
+
+def taylor_expansion_pot_at_05(q, j ,b):
+    k = 6*(j+b)
+    order_0 = np.log(2)
+    order_1 = k*(q-0.5)
+    order_2 = 0.5 * (k**2) * (q-0.5) ** 2
+    order_3 = 0
+    order_4 = -1/12 * (k**4) * (q-0.5) ** 4
+    expansion = 0.5*q**2 - (order_0+order_1+order_2+order_3+order_4) / (2*k)
+    return expansion
+
+
+def plot_potentials_mf(j_list, bias=0):
+    # colormap = pl.cm.Blues(np.linspace(0.2, 1, len(j_list)))
+    # colormap_1 = pl.cm.Purples(np.linspace(0.4, 1, len(j_list)))
     Blues = pl.cm.get_cmap('Blues', 100)
     Purples = pl.cm.get_cmap('Purples', 100)
     newcolors = Blues(np.linspace(0.2, 1, len(j_list)))
@@ -133,8 +382,9 @@ def plot_potentials_mf(j_list):
     fig, ax = plt.subplots(1)
     change_colormap = False
     for i_j, j in enumerate(j_list):
-        pot = potential_mf(q, j)
-        norm_cte = np.max(np.abs(pot))
+        # pot = potential_mf(q, j, bias=bias)
+        pot = potential_expansion_any_order_any_point(q, j, b=0, order=8, point=0.5)
+        # norm_cte = np.max(np.abs(pot))
         if abs(j - 0.333) < 0.01 and not change_colormap:
             color = 'r'
             change_colormap = True
@@ -144,16 +394,16 @@ def plot_potentials_mf(j_list):
     ax_pos = ax.get_position()
     ax_cbar = fig.add_axes([ax_pos.x0+ax_pos.width*1.02, ax_pos.y0,
                             ax_pos.width*0.04, ax_pos.height*0.9])
-    cb = mpl.colorbar.ColorbarBase(ax_cbar, cmap=newcmp)
-    ax_cbar.set_yticks([0, 1/3, 0.5, 1], [0, 0.33, 0.5, 1])
+    mpl.colorbar.ColorbarBase(ax_cbar, cmap=newcmp)
+    ax_cbar.set_yticks([0, max(j_list)/3, max(j_list)/2, max(j_list)], [0, 0.33, max(j_list)/2, max(j_list)])
     ax_cbar.set_title('J')
     ax.set_xlabel('q')
     ax.set_ylabel(r'Mean-centered potential $V_J(q)$')
     # ax.legend(title='J:')
 
 
-def plot_pot_evolution_mfield(j, num_iter=10, sigma=0.1):
-    fig, ax = plt.subplots(ncols=5, nrows=2, figsize=(16, 6))
+def plot_pot_evolution_mfield(j, num_iter=10, sigma=0.1, bias=1e-3):
+    fig, ax = plt.subplots(ncols=5, nrows=3, figsize=(16, 10))
     plt.subplots_adjust(top=0.95, bottom=0.05, left=0.075, right=0.98,
                         hspace=0.3, wspace=0.5)
     # fig.suptitle('J = '+ str(j))
@@ -163,22 +413,22 @@ def plot_pot_evolution_mfield(j, num_iter=10, sigma=0.1):
     m_field = []
     # time = np.arange(num_iter)*dt
     for i in range(num_iter):
-        q = dyn_sys_mf(q, dt=1, j=j, sigma=sigma)
+        q = dyn_sys_mf(q, dt=1, j=j, sigma=sigma, bias=bias)
         m_field.append(q)
-    q = np.arange(-.25,1.25, 0.001)
+    q = np.arange(0, 1., 0.001)
     # q = np.arange(0, 1, 0.001)
-    pot = potential_mf(q, j)
+    pot = potential_mf(q, j, -bias)
     for i in range(len(ax)):
         ax[i].plot(q, pot, color='k')
         q_ind = m_field[i*num_iter//len(ax)]
-        pot_ind = potential_mf(q_ind, j)
+        pot_ind = potential_mf(q_ind, j, -bias)
         ax[i].plot(q_ind, pot_ind, marker='o', color='r')
-        ax[i].set_title('iter' + str(i*num_iter//len(ax)+1))
-        if i != 0 and i != (len(ax)//2):
+        ax[i].set_title('iter #' + str(i*num_iter//len(ax)+1))
+        if i != 0 and i != (2*len(ax)//3) and i != (len(ax)//3):
             ax[i].set_yticks([])
         else:
             ax[i].set_ylabel('Potential')
-        if i < 5:
+        if i < 10:
             ax[i].set_xticks([0, 0.5, 1], ['', '', ''])
         else:
             ax[i].set_xticks([0, 0.5, 1])
@@ -186,7 +436,7 @@ def plot_pot_evolution_mfield(j, num_iter=10, sigma=0.1):
 
 
 if __name__ == '__main__':
-    plot_solutions_mfield(j_list=np.arange(0.00001, 2, 0.005))
+    # plot_solutions_mfield(j_list=np.arange(0.00001, 2, 0.005))
     # for num_iter in [200, 1000, 5000, 10000, 20000, 50000]:
     #     plt.figure()
     #     plot_mf_sol(j_list=np.arange(0.25, 0.65, 0.004), num_iter=num_iter)
@@ -196,4 +446,5 @@ if __name__ == '__main__':
     # plot_mf_sol_sigma_j(data_folder=DATA_FOLDER, j_list=np.arange(0.0001, 1, 0.005),
     #                     sigma_list=np.arange(0, 2, 0.01),
     #                     num_iter=2000)
-    # plot_potentials_mf(j_list=np.arange(0.001, 1, 0.01))
+    plot_potentials_mf(j_list=np.arange(0.001, 1, 0.1), bias=0)
+    # plot_pot_evolution_mfield(j=0.9, num_iter=15, sigma=0.1, bias=0)
