f45b4e42e24828aa0bea5b695089d73ff94623f2
alexgarciaduran <alexgaduca@gmail.com>
2024-07-11 10:19:30 +0200

    tried recurrence VAE

--- variational_autoencoder.py
+++ variational_autoencoder.py
@@ -30,11 +30,36 @@ elif pc_name == 'alex_CRM':
 
 dataset_path = DATA_FOLDER + '/necker_images/'
 
+J = 0.7
+
 cuda = False
 DEVICE = torch.device("cuda" if cuda else "cpu")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} 
 
+
+
+def get_connections(node):
+    if node == 0:
+        return [1, 2, 4]
+    if node == 1:
+        return [0, 3, 5]
+    if node == 2:
+        return [0, 3, 6]
+    if node == 3:
+        return [1, 2, 7]
+    if node == 4:
+        return [0, 5, 6]
+    if node == 5:
+        return [1, 4, 7]
+    if node == 6:
+        return [2, 4, 7]
+    if node == 7:
+        return [3, 5, 6]
+
+
+def sigmoid(x):
+    return 1/(1+torch.exp(-x))
 
 
 class Encoder(nn.Module):
@@ -87,8 +112,11 @@ class Model(nn.Module):
     def reparameterization(self, mean, var):
         epsilon = torch.randn_like(var).to(DEVICE)        # sampling epsilon        
         z = mean + var*epsilon                          # reparameterization trick
+        # z_new = torch.clone(z)
+        # for i in range(len(z)):
+        #     z_new[i] = sigmoid(J*torch.sum(2*z[get_connections(i)]-1))
         return z
-        
+
                 
     def forward(self, x):
         mean, log_var = self.Encoder(x)
@@ -122,7 +150,7 @@ def data_load(dataset_path, batch_size=10):
 
 
 def VAE(dataset_path, batch_size=10, x_dim=5904, hidden_dim=200,
-        latent_dim=8, lr=1e-3, epochs=30):
+        latent_dim=8, lr=1e-3, epochs=30, plot=False):
     encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
     decoder = Decoder(latent_dim=latent_dim, hidden_dim = hidden_dim, output_dim = x_dim)
     model = Model(Encoder=encoder, Decoder=decoder).to(DEVICE)
@@ -152,29 +180,91 @@ def VAE(dataset_path, batch_size=10, x_dim=5904, hidden_dim=200,
     print("Finish!!")
     # model.eval()
     # _, test_data = data_load(dataset_path, batch_size=10)
-    x = test_data.view(batch_size, x_dim)
-    x_hat, _, _, z = model(x)
-    for i in range(4):
-        show_image(x, idx=i)
-        show_image(x_hat, idx=i)
-    plt.figure()
-    z = z.detach().numpy()
-    im = plt.imshow(1/(1+np.exp(-z)))
-    plt.ylabel('Image index')
-    plt.xlabel(r'Hidden variable index, $i$')
-    plt.colorbar(im, label=r'$\sigma(z_i)$')
+    if plot:
+        x = test_data.view(batch_size, x_dim)
+        x_hat, _, _, z = model(x)
+        fig, ax = plt.subplots(ncols=6, nrows=2)
+        ax = ax.flatten()
+        for i in range(6):
+            show_image(x, idx=i, ax=ax[i], lab='data')
+            show_image(x_hat, idx=i, ax=ax[i+6], lab='prediction')
+        plt.figure()
+        z = z.detach().numpy()
+        im = plt.imshow(1/(1+np.exp(-z)))
+        plt.ylabel('Image index')
+        plt.xlabel(r'Hidden variable index, $i$')
+        plt.colorbar(im, label=r'$\sigma(z_i)$')
+    return model
 
 
-def show_image(x, idx, batch_size=10):
+def show_image(x, idx, ax, batch_size=10, lab='',):
     x = x.view(batch_size, 72, 82)
-    plt.figure()
-    plt.imshow(x[idx].detach().numpy(),
-               cmap='gist_gray')
-    plt.title(f'Test image index: {idx}')
+    ax.imshow(x[idx].detach().numpy(),
+              cmap='gist_gray')
+    ax.set_title(f'Test image index: {idx}, ' + lab)
 
+
+def get_image(dataset_path):
+    name = 'fig_76.png'
+    img = plt.imread(dataset_path + name)[:, :, 0]
+    img[img == 1] = 0
+    img[img > 0] = 1
+    image = img
+    image_torch = torch.from_numpy(image).to(torch.float32)
+    return image_torch
+
+
+def recurrent_image(dataset_path, n_iter=50, steps=10):
+    model = VAE(dataset_path, batch_size=10, x_dim=5904, hidden_dim=1200,
+                latent_dim=12, lr=1e-4, epochs=40, plot=False)
+    x_im = get_image(dataset_path)
+    x = torch.clone(x_im)
+    for i in range(9):
+        x = torch.dstack((x_im, x))
+    ncols = n_iter // (2*steps)+1
+    nrows = n_iter // steps // ncols+1
+    fig, ax = plt.subplots(ncols=ncols, nrows=nrows)
+    ax = ax.flatten()
+    x = torch.transpose(torch.transpose(x, 1, 2), 0, 1)
+    ax[0].imshow(x[0].detach().numpy(),
+              cmap='gist_gray')
+    x = x.view(10, 72*82)
+    c = 0
+    for i in range(n_iter+steps):
+        x, _, _, z = model(x)
+        if (i+1) % steps == 0:
+            c += 1
+            x = x.view(10, 72, 82)
+            ax[c].imshow(x[0].detach().numpy(),
+                      cmap='gist_gray')
+            x = x.view(10, 72*82)
 
 
 if __name__ == '__main__':
-    VAE(dataset_path, batch_size=10, x_dim=5904, hidden_dim=1200,
-        latent_dim=8, lr=1e-4, epochs=50)
+    # recurrent_image(dataset_path, n_iter=1000, steps=50)
     # lr was 1e-3
+    n_iter = 10000
+    steps = 1000
+    model = VAE(dataset_path, batch_size=10, x_dim=5904, hidden_dim=1200,
+                latent_dim=12, lr=1e-4, epochs=40, plot=False)
+    x_im = get_image(dataset_path)
+    x = torch.clone(x_im)
+    for i in range(9):
+        x = torch.dstack((x_im, x))
+    ncols = n_iter // (2*steps)+1
+    nrows = n_iter // steps // ncols+1
+    fig, ax = plt.subplots(ncols=ncols, nrows=nrows)
+    ax = ax.flatten()
+    x = torch.transpose(torch.transpose(x, 1, 2), 0, 1)
+    ax[0].imshow(x[0].detach().numpy(),
+              cmap='gist_gray')
+    x = x.view(10, 72*82)
+    c = 0
+    for i in range(n_iter+steps):
+        x, _, _, z = model(x)
+        if (i+1) % steps == 0:
+            c += 1
+            x = x.view(10, 72, 82)
+            ax[c].imshow(x[0].detach().numpy(),
+                      cmap='gist_gray')
+            x = x.view(10, 72*82)
