7f19bb63632517e63d046031995972d23c180062
alexgarciaduran <alexgaduca@gmail.com>
2025-06-02 15:40:17 +0200

    changed density plot ticks

--- fitting_pipeline.py
+++ fitting_pipeline.py
@@ -1249,10 +1249,10 @@ def plot_density(num_iter=100, extra='', method='BADS', model='MF',
     ax.legend(frameon=False)
     ax.plot(b_crit2, j_list, color='k', linewidth=2.5)
     ax.set_xlim(-1.25, 1.25)
-    ax.set_ylim(0, 2.1)
-    ax.set_yticks([0, 1, 2])
+    ax.set_ylim(0, 1.1)
+    ax.set_yticks([0, 0.5, 1])
     ax.set_xticks([-1, 0, 1])
-    ax.text(-1., 2., 'Bistable')
+    ax.text(-1., 1., 'Bistable')
     ax.text(-1., 0.1, 'Monostable')
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
@@ -2572,13 +2572,13 @@ if __name__ == '__main__':
     # plot_conf_vs_coupling_3_groups(method=opt_algorithm, model='MF5', extra='', bw=0.7,
     #                                 data_only=False)
     # plot_bic_across_models(sv_folder=SV_FOLDER, bic=True, method='BADS')
-    # plot_density(num_iter=100, model='MF5', extra='', method=opt_algorithm)
+    plot_density(num_iter=100, model='MF5', extra='', method=opt_algorithm)
     # plot_density(num_iter=100, model='MF', extra='null', method=opt_algorithm)
     # plot_density_comparison(num_iter=100, method=opt_algorithm, kde=False)
-    plot_density_comparison(num_iter=100, method=opt_algorithm, kde=True, stim_ev_0=True,
-                            variable='aligned_confidence', bw=0.6, model='LBP5')
-    plot_density_comparison(num_iter=100, method=opt_algorithm, kde=True, stim_ev_0=True,
-                            variable='aligned_confidence', bw=0.7, model='MF5')
+    # plot_density_comparison(num_iter=100, method=opt_algorithm, kde=True, stim_ev_0=True,
+    #                         variable='aligned_confidence', bw=0.6, model='LBP5')
+    # plot_density_comparison(num_iter=100, method=opt_algorithm, kde=True, stim_ev_0=True,
+    #                         variable='aligned_confidence', bw=0.7, model='MF5')
     # plot_regression_weights(sv_folder=SV_FOLDER, load=True, model='MF5',
     #                         method=opt_algorithm)
     # ridgeplot_all_subs(sv_folder=SV_FOLDER, model='LBP5', method=opt_algorithm,
